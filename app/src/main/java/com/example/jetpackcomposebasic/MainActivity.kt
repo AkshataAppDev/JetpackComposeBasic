09abab06e1631bafcc502c2ad48c4dc1bb6f7b6c
Akshata Deshpande <ad.march6@gmail.com>
2021-03-31 16:04:47 +0100

    Make use of Container Functions, multiple layouts and for loop example with UI

--- app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,25 +18,29 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
+            MyApp{
+               MyScreenContent()
+            }
         }
     }
 }
 
 
+// Composable functions dont return UI components, they emit them.So they must return Unit
 // contains UI code specific to this activity so Greeting could be reusable
 // Also, MyApp can now be used in multiple places as it defines a top level configuration.
+// MyApp is now a container function.
 @Composable
-fun MyApp()
+fun MyApp(content: @Composable () -> Unit)
 {
     JetpackComposeBasicTheme {  // this name should be as per your project name
         // A surface container using the 'background' color from the theme
-        Surface(color = Color.Yellow) {
-            Greeting(name = "Android")
+        Surface(color = Color.Yellow) { 
+           content()
         }
     }
 }
@@ -45,8 +52,22 @@ fun Greeting(name: String) {
         Text(text = "Hello $name!", modifier = Modifier.padding(24.dp)) // will be drawn on the surface color
 }
 
-@Preview // Preview mark when any parameterless function
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android","There !!"))
+{
+    Column {
+        // UI elements are emitted by calling Composable functions
+        for(name in names)
+        Greeting(name = name)
+        Divider(color = Color.Black)
+    }
+}
+
+@Preview("MyScreen Preview") // Preview mark when any parameterless function to show preview of UI in Design or split view
 @Composable
 fun DefaultPreview() {
-    MyApp()
+    MyApp {
+        MyScreenContent()
+    }
+    
 }
